75994bbc307cc4746f3f45eddc4f93e0e0bf7763
Soyann Chauvin <soyann19@gmail.com>
2026-09-03 18:24:49 +0200

    Streamlit

---
+++ app/Hello.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import requests
+import pandas as pd
+from datetime import date, time
+import numpy as np
+
+
+# spell = st.secrets['spell']
+# key = st.secrets.some_magic_api.key
+
+
+st.title("PIAFZAM 🕊️")
+st.caption("Quelle espèce d'oiseau est en train de chanter 🎵 ?")
